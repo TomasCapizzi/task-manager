52d29e8ef285d5f3efb6b7b1cb60febc32e4cb1b
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-01-03 17:42:25 -0300

    Adapting Bulma

--- src/Components/TareasEnProceso.jsx
+++ src/Components/TareasEnProceso.jsx
@@ -4,19 +4,19 @@ import { connect } from "react-redux";
 function TareasEnProceso({enProceso, eliminar,volverAtras, finalizar}){
 
     return(
-        <article>
-            <h2>En proceso</h2>
-            <div>
+        <article className="column content"> 
+            <h2 className="title has-text-centered">En proceso</h2>
+            <div className="my-3 is-flex is-flex-wrap-wrap">
                 {
                     enProceso.map(
                         tarea => (
-                            <article key={tarea.id}>
+                            <article key={tarea.id} className='has-background-primary-light p-3 is-flex is-flex-direction-column is-align-items-center m-2'>
+                                <h4 className="title has-text-primary">{tarea.nombre}</h4>
                                 <div>
-                                    <button onClick={()=> volverAtras(tarea)}>Volver atrás</button>
-                                    <button onClick={()=> finalizar(tarea) }>Finalizar</button>
-                                    <button onClick={()=> eliminar(tarea)} >X</button>
+                                    <button onClick={()=> volverAtras(tarea)} className="button is-link is-small is-outlined" >Volver atrás</button>
+                                    <button onClick={()=> finalizar(tarea)} className="button is-info is-small is-outlined">Finalizar</button>
+                                    <button onClick={()=> eliminar(tarea)} className="button is-danger is-small" >X</button>
                                 </div>
-                                <p>{tarea.nombre}</p>
                             </article>
                         )
                     )
