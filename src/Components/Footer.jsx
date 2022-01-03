9ed0629aeb8871b58f95ffe6abbc12878bc3136d
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-01-03 17:53:29 -0300

    Create Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer class="footer has-background-link">
+            <div class="content has-text-centered">
+                <p className='has-text-white-ter'>
+                <strong>Task Manager</strong> by <a href="https://tomascapizzi-portfolio.netlify.app/" target='_blank' rel="noreferrer" className='has-text-warning'>Tomás Capizzi</a>. React application using Redux and Bulma Css
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
