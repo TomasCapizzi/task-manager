7c5708eee1f73544b610415ef9da9db24547c0be
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-01-03 14:57:13 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import EstadosTarea from './Components/EstadosTarea';
+//El componente proveniente de react-redux. Envuelve toda la app
+import {Provider} from 'react-redux';
+import React from 'react';
+import Tareas from './Components/Tareas';
+// Lo que vamos a proveer, en este caso el store.js
+import store from './store';
+
+// Task Manager: una app para manejar tareas y tener un control, onda trello. Lo que se completo, lo que falta, lo que esta en progreso. 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Task Manager</h1>
+        <Tareas/>
+        <EstadosTarea/>
+      </div>
+    </Provider>
   );
 }
 
