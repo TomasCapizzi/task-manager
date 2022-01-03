7b7d8e5124990cbd6b55175688a6ddfcea16daca
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-01-03 17:53:26 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import 'bulma/css/bulma.min.css'
 
 import EstadosTarea from './Components/EstadosTarea';
+import Footer from './Components/Footer';
 //El componente proveniente de react-redux. Envuelve toda la app
 import {Provider} from 'react-redux';
 import React from 'react';
@@ -11,11 +12,12 @@ import store from './store';
 function App() {
   return (
     <Provider store={store}>
-      <div className='container is-fluid is-flex-wrap-wrap is-justify-content-center is-gapless' style={{height: '100vh'}}>
+      <div className='container is-fluid is-flex-wrap-wrap is-justify-content-center is-gapless p-0' style={{height: '100vh'}}>
         <section className="">
           <h1 className='title has-text-primary py-4 has-text-centered'>Task Manager</h1>
           <EstadosTarea/>
         </section>
+        <Footer/>
       </div>
     </Provider>
   );
